c6759ed0ce238aab9c3d131fc757399ea36d2a8f
Anas <anas@senangpay.my>
2024-04-06 11:36:49 +0800

    changes

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -3,6 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class LoginController extends CI_Controller {
 
+    public $dynamic_property;
+
     public function __construct() {
         parent::__construct();
         $this->load->model('Auth');
